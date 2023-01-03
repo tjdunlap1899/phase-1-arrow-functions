c94a840036b4adc07b9d21c23b6b4d4d0dcb08ce
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-01-03 11:05:45 -0500

    commit tree 2f88eea3dbec02d269f2a2efb7f3f11caad29e76

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    return a / b;
+}
+console.log(divide(2000, 100))
+
+let square = x => x * x;
+console.log(square(2))
+
+let add = (a, b) => a + b;
+console.log(add(3, 4));
